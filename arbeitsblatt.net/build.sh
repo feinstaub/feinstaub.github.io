72763042836e5d030b684b861ace484e2296a7d4
Gregor Mi <codestruct@posteo.org>
2015-12-12 22:38:51 +0100

    update arbeitsblatt

--- arbeitsblatt.net/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# For syntax-highlighting
-# sudo zypper install source-highlight
-
-# AsciiDoc ships with two themes: flask and volnitsky.
-asciidoc -b html5 -a icons -a toc2 -a --theme=flask *.adoc
